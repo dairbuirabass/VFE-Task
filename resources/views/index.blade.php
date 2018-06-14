a246eea283b921d402d26032a582f0673b782db4
Unknown <dair.baidauletov@gmail.com>
2018-06-14 04:31:06 +0300

    Environment setup done

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Vaimo Store</title>
+    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <!-- Header -->
+      <div class="container">
+
+      </div>
+    <!-- End of Header -->
+  </body>
+</html>
